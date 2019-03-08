c03ed118cd868e0b9a8dde8a71e0724d2343056e
julia <julia.fvila@hotmail.com>
2019-03-08 16:55:19 -0300

    iniciando implementação do pulo

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,8 +2,12 @@ extends KinematicBody2D
 
 const GRAVITY = 200.0
 const WALK_SPEED = 200
+const JUMP_SPEED = -200
+const MIN_JUMP = -500
 
 var velocity = Vector2()
+
+onready var tile_color = get_node("")
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -11,17 +15,19 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-    velocity.y += delta * GRAVITY
+	velocity.y += delta * GRAVITY
 
-    if Input.is_action_pressed("ui_left"):
-        velocity.x = -WALK_SPEED
-    elif Input.is_action_pressed("ui_right"):
-        velocity.x =  WALK_SPEED
-    else:
-        velocity.x = 0
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -WALK_SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x =  WALK_SPEED
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y = JUMP_SPEED
+	else:
+		velocity.x = 0
 
     # We don't need to multiply velocity by delta because MoveAndSlide already takes delta time into account.
 
     # The second parameter of move_and_slide is the normal pointing up.
     # In the case of a 2d platformer, in Godot upward is negative y, which translates to -1 as a normal.
-    move_and_slide(velocity, Vector2(0, -1))
+	move_and_slide(velocity, Vector2(0, -1))
