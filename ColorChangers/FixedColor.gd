47fbfdcf2cb8a5c38fa67ded5cbe8a9222f037e1
Júlia Vila <juliafvila@gmail.com>
2019-04-06 10:34:18 -0300

    concluindo implementação dos ColorChangers

--- ColorChangers/FixedColor.gd
+++ ColorChangers/FixedColor.gd
@@ -1,13 +1,9 @@
 extends Area2D
 
-func _on_captured_color_body_entered(body):
+func _on_fixed_color_body_entered(body):
 	
 	var my_group = get_groups()[0]
 	var	body_group = body.get_groups()[0]
 	
-	add_to_group(body_group)
-	remove_from_group(my_group)
-	
 	body.add_to_group(my_group)
 	body.remove_from_group(body_group)
-	
